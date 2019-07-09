dcf4560cd05285398551cfd5c5dae2278728d4d7
Carl Wood <cjwood17@googlemail.com>
2019-07-09 20:40:12 +0100

    Simple todo list where you can check off items and be told when you've completed all of them

--- src/App.svelte
+++ src/App.svelte
@@ -8,23 +8,37 @@
 </script>
 
 <style>
-	.body {
-		background-color: #efefef;
+	:global(body) {
+		/* this will apply to <body> */
+		margin: 0;
+		padding: 0;
 	}
+
+	:global(*) {
+		box-sizing: border-box;
+	}
+
 	.app {
 		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 		font-size: 18px;
-		max-width: 960px;
+		max-width: 1200px;
 		margin: 0 auto;
+		padding: 0 0.5rem;
 	}
+
+	@media (min-width: 768px) {
+		.app {
+			font-size: 24px;
+		}
+	}
+
 	h1 {
 		color: purple;
+		margin-top: 2rem;
 	}
 </style>
 
-<div class="body">
-	<div class="app">
-		<h1>Todos</h1>
-		<TodoList />
-	</div>
+<div class="app">
+	<h1>Todos</h1>
+	<TodoList />
 </div>
--- src/TodoList.svelte
+++ src/TodoList.svelte
@@ -4,36 +4,63 @@
 
     let todoItems = [
         { title: 'Wash the car', done: true },
-        { title: 'Feed the dog', done: false }
+        { title: 'Feed the dog', done: false },
+        { title: 'Pick up the kids', done: false },
+        { title: 'Beat high score', done: false },
+        { title: 'Beat new high score', done: false },
     ]
+
+    let allItems = [];
+
 </script>
 
 <style>
-    .is-done {
-        background: lightgreen;
-    }
-
-    label {
-        padding: 1rem;
-        width: 100%;
-        cursor: pointer;
-    }
-
-    ul {
+    .todo-list {
         list-style: none;
         margin: 0;
         padding: 0;
+        border-top: solid 5px #333;
+    }
+
+    .todo-list__item {
+        border-bottom: solid 1px #aaa;
+    }
+
+    .todo-list__label {
+        padding: 1rem;
+        width: 100%;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+    }
+
+    @media (min-width: 768px) {
+        .todo-list__label {
+            padding: 1.5rem;
+        }
+    }
+
+    .isDone {
+        background: lightgreen;
+        text-decoration: line-through;
+    }
+
+    input {
+        margin: 0 1rem 0 0;
     }
 </style>
 
-<ul>
-    
+<ul class="todo-list">
     {#each todoItems as item, index }
-        <li class="{item.done ? 'is-done' : ''}">
-            <label for="{index}">
+        <li class="todo-list__item" class:isDone={item.done}>
+            <label class="todo-list__label" for="{index}">
+                <input id="{index}" value={item.title} type="checkbox" bind:group={allItems} bind:checked={item.done} />
                 {item.title}
-                <input id="{index}" type="checkbox" bind:checked={item.done} />
             </label>
         </li>
     {/each}
 </ul>
+
+{#if allItems.length === todoItems.length }
+    <p>All items complete!</p>
+{/if }
